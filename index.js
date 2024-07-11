628454c67aff65ae206b2fd9709d2efbe9c31f9e
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2024-07-11 13:10:15 +0700

    index.js change test function

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ app.use("/api/auth/article", ArticleLikeRouter);
 
 // test route
 app.get("/", async (req, res) => {
-    // const article_user = await ArticleModel.find().populate("user", "name");
-    // return res.json(article_user);
-    res.json("Deployment is Okay!!!");
+    const article_user = await ArticleModel.find().populate("user", "name");
+    return res.json(article_user);
+    // res.json("Deployment is Okay!!!");
 });
 
 app.listen(1234, () => {
